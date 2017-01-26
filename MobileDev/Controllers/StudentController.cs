908ee9d15c6fe4742c26169f6b429b420056ea13
nathanmante@yahoo.co.uk <nathanmante@yahoo.co.uk>
2017-01-25 23:58:57 -0500

    I added the asp.net assignment

---
+++ MobileDev/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MobileDev.Models;
+
+namespace MobileDev.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult studentInfo()
+        {
+            studentDetails newStud = new studentDetails();
+
+            newStud.Name = "Nathan Mante";
+            newStud.sNumber = "N01089246";
+            newStud.program = "Web Development";
+
+            return View(newStud);
+        }
+    }
+}
---
+++ MobileDev/Models/studentDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileDev.Models
+{
+    public class studentDetails
+    {
+        //Properties
+        public string Name { get; set; }
+        public string sNumber { get; set; }
+        public string program { get; set; }
+
+    }
+}
